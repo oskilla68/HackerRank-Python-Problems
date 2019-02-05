32e64dfc83e774bff4a8d6965346092b61a0197f
oskilla68 <30677916+oskilla68@users.noreply.github.com>
2019-02-05 16:42:45 +1100

    Update with improved solution with Lambda function
    What I learnt:
    > checks for proper/strict superset
    Lambda functions, combined with mapping allows for convenient and shorter code
    Lambda functions simplify functions, where x is the parameter and is followed by the expression
    This is followed by what should be mapped/passed into the lambda function, which is a list of sets of subsets

--- strict_superset_check.py
+++ strict_superset_check.py
@@ -1,11 +1,18 @@
 # Strict Superset Check
 super_set = list(map(int, input().split()))
-amount = int(input())
-while amount > 0:
-    sub_set = list(map(int, input().split()))
-    for number in sub_set:
-        if super_set.count(number) == 0:
-            print(False)
-            exit()
-    amount -= 1
-print(True)
+# amount = int(input())
+# while amount > 0:
+#     sub_set = list(map(int, input().split()))
+#     for number in sub_set:
+#         if super_set.count(number) == 0:
+#             print(False)
+#             exit()
+#     amount -= 1
+# print(True)
+
+# A better approach
+# > checks for proper/strict superset
+# Lambda functions, combined with mapping allows for convenient and shorter code
+# Lambda functions simplify functions, where x is the parameter and is followed by the expression
+# This is followed by what should be mapped/passed into the lambda function, which is a list of sets of subsets
+print(all(map(lambda x: (super_set > x), [set(map(int, input().split())) for i in range(int(input()))])))
